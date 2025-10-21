a7c331ba35e454cf86e5efd2916eaa345820430e
Steven Spier <10742399+sspier@users.noreply.github.com>
2025-10-21 15:25:07 -0400

    Refactor StopWords to use Set and improve file handling
    Refactor StopWords class to use a Set for stop words and improve file reading logic.

--- Utilities/StopWords.java
+++ Utilities/StopWords.java
@@ -1,43 +1,57 @@
 /*
  * Steven Spier
  */
-package Utilities;
+package utilities;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StopWords {
-    
-    private static StopWords instance;
-    private final File f = new File("stopwords.txt");
-    ArrayList<String> stopWords;
-    
-    private StopWords() throws FileNotFoundException, IOException {
-        System.out.println("### reading the stopwords file");
-        stopWords = new ArrayList<>();
-        BufferedReader readStopWords = new BufferedReader(new FileReader(f));
-        String stopLine = readStopWords.readLine();
-        Scanner sc = new Scanner(stopLine);
-        sc.useDelimiter(",");
-        while (sc.hasNext()) {
-            stopWords.add(sc.next());
+
+    private static volatile StopWords instance;
+    private final Set<String> stopWords;
+
+    private StopWords(File file) throws IOException {
+        System.out.println("### Reading stopwords file: " + file.getAbsolutePath());
+        Set<String> words = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (String word : line.split(",")) {
+                    if (!word.isBlank()) {
+                        words.add(word.trim().toLowerCase());
+                    }
+                }
+            }
         }
+        this.stopWords = Collections.unmodifiableSet(words);
     }
-    
-    public static StopWords getInstance() throws FileNotFoundException, IOException {
+
+    public static StopWords getInstance() throws IOException {
+        return getInstance(new File("stopwords.txt"));
+    }
+
+    public static StopWords getInstance(File file) throws IOException {
         if (instance == null) {
-            instance = new StopWords();
+            synchronized (StopWords.class) {
+                if (instance == null) {
+                    instance = new StopWords(file);
+                }
+            }
         }
         return instance;
     }
-    
-    public ArrayList<String> get() {
+
+    public Set<String> get() {
         return stopWords;
     }
-    
+
+    public boolean contains(String word) {
+        return stopWords.contains(word.toLowerCase());
+    }
 }
